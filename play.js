ebda54f093b6edd89ec1a4a75aabdb1c8a318ff3
Nolan Eckert <nolan.eckert@gmail.com>
2021-02-10 15:34:11 -0700

    Updated play.js - Setup user input through stdin and added an escape callback to allow ctrl + c to exit the program.

--- play.js
+++ play.js
@@ -1,5 +1,23 @@
 // IMPORT MODULES
 const { connect } = require('./client');
 
+const handlUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf-8');
+  stdin.resume();
+  stdin.on('data', handlUserInput);
+  return stdin;
+};
+
+
 console.log('Connecting ...');
+
 connect();
+setupInput();
