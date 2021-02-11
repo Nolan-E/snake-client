2c7ce836845c7181f0e642619e949e62d8605c8e
Nolan Eckert <nolan.eckert@gmail.com>
2021-02-10 19:22:08 -0700

    Added directional movement to the snake game. Stdin takes inputs without errors and writes to the server.

--- input.js
+++ input.js
@@ -1,17 +1,36 @@
+// IMPORT MODULES
+const { stdin } = require("process");
+
+let connection;
+
 const handlUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
   }
 };
 
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf-8');
   stdin.resume();
   stdin.on('data', handlUserInput);
+  stdin.on('data', (key) => {
+    if (key === 'w') {
+      conn.write('Move: up');
+    } else if (key === 'a') {
+      conn.write('Move: left');
+    } else if (key === 's') {
+      conn.write('Move: down');
+    } else if (key === 'd') {
+      conn.write('Move: right');
+    }
+  });
+
   return stdin;
 };
+
 
 // EXPORT MODULES
 module.exports = {
